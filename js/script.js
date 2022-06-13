b8f1693686f096f339f3c3e4eff2de2f6eec0b47
Hoang <hoangtl@timebird.org>
2022-06-13 14:28:00 +0700

    update

--- js/script.js
+++ js/script.js
@@ -25,3 +25,27 @@ setTimeout(function() {
   })
 })(jQuery)
 
+
+function animateNumber(finalNumber, delay, startNumber = 0, callback) {
+  let currentNumber = startNumber
+  const interval = window.setInterval(updateNumber, delay)
+  function updateNumber() {
+    if (currentNumber >= finalNumber) {
+      clearInterval(interval)
+    } else {
+      currentNumber++
+      callback(currentNumber)
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  animateNumber(32, 100, 0, function (number) {
+    const formattedNumber = number.toLocaleString()
+    document.getElementById('count-number').innerText = formattedNumber
+  })
+});
+
+
+
+
